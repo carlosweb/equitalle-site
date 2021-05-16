eb8d37cb23a9ac27415271c40c135ad9fa025e15
Carlos Eduardo <kaduinfo@gmail.com>
2021-05-16 11:24:14 -0300

    commit test

--- comps/Footer.js
+++ comps/Footer.js
@@ -1,6 +1,6 @@
 import styles from '../styles/Footer.module.scss'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"; // Import the FontAwesomeIcon component
-import { faWhatapp } from "@fortawesome/free-solid-svg-icons";
+import { faAddressBook } from "@fortawesome/free-solid-svg-icons";
 import { faMailBulk } from "@fortawesome/free-solid-svg-icons";
 
 export default function Footer() {
@@ -9,7 +9,7 @@ export default function Footer() {
             <footer className={styles.footer}>
                 <div className={styles.info}>
                     <h1>Do you have any question? <br /> Feel free to contact</h1>
-                    <h3><FontAwesomeIcon icon={faWhatapp}></FontAwesomeIcon> +22 025210-805045</h3>
+                    <h3><FontAwesomeIcon icon={faAddressBook}></FontAwesomeIcon> +22 025210-805045</h3>
                     <h3><FontAwesomeIcon icon={faMailBulk}></FontAwesomeIcon> contato@equitalle.com.br</h3>
                 </div>
             </footer>
